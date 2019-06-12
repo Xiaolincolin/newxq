3d983cfc2602321543dcaae813f76543392c0553
小林 <1144759420@qq.com>
2019-06-12 13:36:40 +0800

    delete

--- newxq/apps/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-author: Colin
-@time: 2019-06-11 22:50
-explain:
-
-"""
- 
--- newxq/apps/reposityory/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReposityoryConfig(AppConfig):
-    name = 'reposityory'
-    verbose_name = "资讯墙"
